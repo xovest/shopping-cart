89d2ae9e9b6dbe8a77633bee5219fffa22ca9dae
xovest <79008917+xovest@users.noreply.github.com>
2021-11-06 13:36:34 +0200

    cart added

---
+++ src/Components/Cart.js
@@ -0,0 +1,35 @@
+import { useContext } from 'react';
+import CartContext from '../context/cart/CartContext';
+import formatCurrency from 'format-currency';
+
+const Cart = () => {
+  const { showCart, cartItems, showHideCart } = useContext(CartContext);
+  let options = {format: '%s%v', symbol: '$' };
+
+  return (
+    <>
+    {showCart && (
+      <div className="cart__wrapper">
+        <div style={{ testAlign: 'right' }}>
+          <i style={{ cursor: 'pointer' }} className="fa fa-times-circle" aria-hidden="true" onClick={showHideCart}></i>
+        </div>
+        <div className="cart__innerWrapper">
+          {cartItems.length === 0 ? <h4>Cart is Empty</h4> : <ul>{cartItems.map(item => (
+            <CartItem key={item._id} item={item} />
+          ))}</ul>}
+        </div>
+        <div className="Cart__cartTotal">
+          <div>
+            Cart Total
+          </div>
+          <div style={{ marginLeft: 5 }}>
+            {formatCurrency(cartItems.reduce((amount, item) => item.price + amount, 0), options)}
+          </div>
+        </div>
+      </div>
+    )}
+    </>
+  );
+}
+
+export default Cart;
